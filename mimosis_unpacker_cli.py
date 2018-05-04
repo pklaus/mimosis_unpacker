7473b1c76d2f4cce125326dd83fa6f032a3499f6
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2018-05-04 17:36:45 +0200

    Use click.group() for CLI subcommands

--- mimosis_unpacker_cli.py
+++ mimosis_unpacker_cli.py
@@ -10,18 +10,28 @@ import click
 import queue, threading
 
 
-@click.command()
-@click.option('--host', help='Host to listen on.')
+@click.group()
+@click.option('--host', required=True, help='Host to listen on.')
 @click.option('--port', default=40000, help='Port to listen on.')
 @click.option('--buffer-size', default=9000, help='Buffer size for the UDP packets.')
-def run(host, port, buffer_size):
+@click.pass_context
+def cli(ctx, host, port, buffer_size):
+    ctx.obj['HOST'] = host
+    ctx.obj['PORT'] = port
+    ctx.obj['BUFFER_SIZE'] = buffer_size
+
+@cli.command()
+@click.pass_context
+@click.option('--interval', default=1.0, help='The interval to print the stats.')
+def timed_stats(ctx, interval):
     q = queue.Queue()
     # start recv thread
-    recv_thread = threading.Thread(target=mimosis_unpacker.udp_receive, args=(host, port, q, buffer_size))
+    args = (ctx.obj['HOST'], ctx.obj['PORT'], q, ctx.obj['BUFFER_SIZE'])
+    recv_thread = threading.Thread(target=mimosis_unpacker.udp_receive, args=args)
     recv_thread.start()
     # start print thread
-    stats_thread = threading.Thread(target=mimosis_unpacker.timed_queue_stats, args=(q, ))
+    stats_thread = threading.Thread(target=mimosis_unpacker.timed_queue_stats, args=(q, interval))
     stats_thread.start()
 
 
-if __name__ == "__main__": run()
+if __name__ == "__main__": cli(obj={})
