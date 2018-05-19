885cd1ab3d1fcaf37c8e52b9a6dbdb25a5630724
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2018-05-19 16:09:49 +0200

    split off mimosis_unpacker.output from  mimosis_unpacker.cli

--- mimosis_unpacker/cli.py
+++ mimosis_unpacker/cli.py
@@ -2,22 +2,14 @@
 
 # internal modules
 import mimosis_unpacker
+import mimosis_unpacker.output
 
 # external deps
 import click
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import scipy.misc
 
 # Python stdlib
-import queue, threading, time, os
-from os import path
-
-def add_to_file_name(old_name, addition):
-    fragments = path.splitext(old_name)
-    return fragments[0] + addition + fragments[1]
+import queue, threading, time
 
 @click.group()
 @click.option('--host', required=True, help='Host to listen on.')
@@ -62,33 +54,7 @@ def matrix_image(ctx, filename):
         print("Exiting...")
     stop_event.set()
     fill_matrix_thread.join()
-    m = m.astype(np.float)
-    m = m[0:32, :]
-    m_nan = m.copy()
-    m_nan[m_nan == 0.] = np.nan
-    # save matplotlib plot
-    plt.figure(figsize=(40,10))
-    plt.imshow(m_nan, interpolation='nearest')
-    plt.colorbar()
-    plt.savefig(filename)
-    print("saved", filename)
-    # save 1:1 image
-    # https://docs.scipy.org/doc/scipy-0.16.1/reference/generated/scipy.misc.imsave.html
-    # https://docs.scipy.org/doc/scipy-0.16.1/reference/generated/scipy.misc.toimage.html
-    m = np.log10(m_nan)
-    m = np.interp(m, (np.nanmin(m), np.nanmax(m)), (0, 255))
-    np.nan_to_num(m, copy=False)
-    m = m.astype(np.uint64)
-    print(m)
-    newname = add_to_file_name(filename, "_scipy")
-    scipy.misc.imsave(newname , m)
-    print("saved", newname)
-    oldname = newname
-    newname = add_to_file_name(oldname, "_color")
-    os.system("to_color_scale -s tillscale {}".format(oldname))
-    oldname = newname
-    newname = add_to_file_name(oldname, "_3x6")
-    os.system("convert {} -scale 300%x600% {}".format(oldname, newname))
+    mimosis_unpacker.output.save_matrix_outputs(m, filename)
 
 def main():
     cli(obj={})
